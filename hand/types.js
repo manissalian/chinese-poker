82928f61a9ca3b56a207ab458410445a17f25314
Raffi <rmanisalian@cinemoz.com>
2020-03-21 14:33:30 +0100

    all hand types added

--- hand/types.js
+++ hand/types.js
@@ -1,38 +1,102 @@
-module.exports = {
-  single: (hand) => {
-    return hand.length === 1
-  },
-  pair: (hand) => {
-    return hand.length === 2 &&
-            hand[0].value === hand[1].value
-  },
-  threeOfAKind: (hand) => {
-    return hand.length === 3 &&
-            hand[0].value === hand[1].value &&
-            hand[0].value === hand[2].value
-  },
-  // from here onwards the level order should be in descending order
-  flush: (hand) => {
-    if (hand.length !== 5) return false
+const single = (hand) => {
+  return hand.length === 1
+}
 
-    return hand[0].category === hand[1].category &&
-            hand[0].category === hand[2].category &&
-            hand[0].category === hand[3].category &&
-            hand[0].category === hand[4].category
-  },
-  straight: (hand) => {
-    const _hand = hand.slice()
+const pair = (hand) => {
+  return hand.length === 2 &&
+          hand[0].value === hand[1].value
+}
 
-    if (_hand.length !== 5) return false
+const threeOfAKind = (hand) => {
+  return hand.length === 3 &&
+          hand[0].value === hand[1].value &&
+          hand[0].value === hand[2].value
+}
 
-    _hand.sort((cardA, cardB) => {
-      return cardA.value > cardB.value ? 1 : -1
-    })
+const straight = (hand) => {
+  if (hand.length !== 5) return false
 
-    for (let i = 0; i < 4; i += 1) {
-      if (_hand[i].value !== _hand[i + 1].value - 1) return false
-    }
+  const sortedHand = sortHand(hand)
 
+  for (let i = 0; i < 4; i += 1) {
+    if (sortedHand[i].value !== sortedHand[i + 1].value - 1) return false
+  }
+
+  return true
+}
+
+const flush = (hand) => {
+  if (hand.length !== 5) return false
+
+  return hand[0].category === hand[1].category &&
+          hand[0].category === hand[2].category &&
+          hand[0].category === hand[3].category &&
+          hand[0].category === hand[4].category
+}
+
+const fullHouse = (hand) => {
+  if (hand.length !== 5) return false
+
+  const sortedHand = sortHand(hand)
+
+  if (sortedHand[0].value === sortedHand[1].value &&
+      sortedHand[0].value === sortedHand[2].value) {
+    return sortedHand[3].value === sortedHand[4].value
+  } else if (sortedHand[2].value === sortedHand[3].value &&
+              sortedHand[2].value === sortedHand[4].value) {
+    return sortedHand[0].value === sortedHand[1].value
+  }
+
+  return false
+}
+
+const fourOfAKind = (hand) => {
+  if (hand.length !== 5) return false
+
+  const sortedHand = sortHand(hand)
+
+  if (sortedHand[0].value === sortedHand[1].value &&
+      sortedHand[0].value === sortedHand[2].value &&
+      sortedHand[0].value === sortedHand[3].value) {
+    return true
+  } else if (sortedHand[1].value === sortedHand[2].value &&
+              sortedHand[1].value === sortedHand[3].value &&
+              sortedHand[1].value === sortedHand[4].value) {
     return true
   }
+
+  return false
+}
+
+const straightFlush = (hand) => {
+  return flush(hand) && straight(hand)
+}
+
+const royalFlush = (hand) => {
+  if (!straightFlush(hand)) return false
+
+  const sortedHand = sortHand(hand)
+
+  return sortedHand[4].value === 13
+}
+
+const sortHand = (hand) => {
+  const _hand = hand.slice()
+
+  return _hand.sort((cardA, cardB) => {
+    return cardA.value > cardB.value ? 1 : -1
+  })
+}
+
+module.exports = {
+  single,
+  pair,
+  threeOfAKind,
+  // from here onwards the level order should be in descending order
+  royalFlush,
+  straightFlush,
+  fourOfAKind,
+  fullHouse,
+  flush,
+  straight
 }
